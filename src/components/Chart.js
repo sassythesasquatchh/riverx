a9bd7e0dbc07c22582e2cac9f56b1f140dfaf316
sassythesasquatchh <pcd276@nyu.edu>
2023-04-27 17:58:51 +0400

    working graph

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,89 +1,305 @@
+// import { Autocomplete, TextField } from "@mui/material";
+
+// const axios = require('axios');
+// const pandas = require('pandas-js');
+
+// const url = 'https://7day35xrbospq4slpqt2abr7fq0mzeja.lambda-url.us-east-1.on.aws/';
+
+// axios.get(url)
+//   .then(response => {
+//     const data = response.data;
+//     const df = pandas.DataFrame.fromRecords(data);
+//     console.log(df.head());
+//   })
+//   .catch(error => {
+//     console.error(error);
+//   });
+
+// const handleMeasureValueChange = e => {
+//   setMeasureValue(e.target.value);
+// };
+
+
+
+// const Chart = () => {
+
+//   const [measureValue, setMeasureValue] = useState('');
+
+
+//   return (
+//     <div>
+//       <Autocomplete
+//         className={styles.autocompletestandard}
+//         disablePortal
+//         options={[
+//           "pH",
+//           "Conductivity",
+//           "Light Intensity",
+//           "Temperature",
+//           "Humidity",
+//           "Oxygen",
+//           "CO2",
+//         ]}
+//         renderInput={(params) => (
+//           <TextField
+//             {...params}
+//             color="primary"
+//             label="Label"
+//             variant="standard"
+//             placeholder=""
+//             helperText=""
+//           />
+//         )}
+//         defaultValue="pH"
+//         size="medium"
+//       />
+//     </div>
+//   );
+// };
+
+// export default Chart;
+
+
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import { Line } from 'react-chartjs-2';
+// import { Autocomplete, TextField } from "@mui/material";
+// import styles from "./Chart.module.css";
+// import defaultData from "./defaultData.js";
+
+// function Chart() {
+//   const [data, setData] = useState([defaultData]);
+//   const [dependentVar, setDependentVar] = useState('temperature');
+//   const [filteredData, setFilteredData] = useState([]);
+
+//   useEffect(() => {
+//     axios.get('https://7day35xrbospq4slpqt2abr7fq0mzeja.lambda-url.us-east-1.on.aws/', {
+//     })
+//       .then((response) => {
+        
+//         const newData = response.data.map((d) => {
+//           return {
+//             name: d.Name,
+//             measure_name: d.measure_name,
+//             time: new Date(d.time),
+//             measure_value: parseInt(d['measure_value::bigint'])
+//           };
+//         });
+//         console.log(newData);
+//         setData(newData);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   }, []);
+
+//   useEffect(() => {
+//     setFilteredData(data.filter((d) => d.measure_name === dependentVar));
+//   }, [dependentVar, data]);
+
+//   const chartData = {
+//     labels: filteredData.map((d) => d.time.toLocaleString()),
+//     datasets: [
+//       {
+//         label: dependentVar,
+//         data: filteredData.map((d) => d.measure_value),
+//         fill: false,
+//         borderColor: 'rgba(75,192,192,1)',
+//         borderWidth: 2,
+//       },
+//     ],
+//   };
+
+//   const options = {
+//     scales: {
+//       xAxes: [
+//         {
+//           type: 'time',
+//           distribution: 'linear',
+//           time: {
+//             displayFormats: {
+//               minute: 'h:mm a',
+//               hour: 'hA',
+//               day: 'MMM D',
+//             },
+//           },
+//         },
+//       ],
+//     },
+//   };
+
+//   return (
+//     <div>
+//       <Autocomplete
+//         id="dependent-var"
+//         className={styles.autocompletestandard}
+//         disablePortal
+//         options={Array.from(new Set(data.map((d) => d.measure_name)))}
+//         defaultValue="temperature"
+//         onChange={(event, newValue) => {
+//           // setDependentVar(newValue);
+//           setFilteredData(data.filter((d) => d.measure_name === dependentVar));
+//         }}
+//         renderInput={(params) => (
+//           <TextField
+//             {...params}
+//             color="primary"
+//             label="Parameter"
+//             variant="standard"
+//             placeholder=""
+//             helperText=""
+//           />
+//         )}
+//       />
+//       <Line data={chartData} options={options} />
+//     </div>
+//   );
+// }
+
+// export default Chart;
+
+// import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
+// import { Autocomplete, TextField } from "@mui/material";
+// import * as d3 from 'd3';
+// import Plot from 'react-plotly.js';
+
+// function unpack(rows, key) {
+//   return rows.map(function (row) {
+//     return row[key];
+//   });
+// }
+
+// const Chart = () => {
+
+//   const [data, setData] = useState([]);
+//   const [plotData, setPlotData] = useState([]);
+//   const [selectedY, setSelectedY] = useState('temperature');
+
+ 
+
+//   // Fetch data from the API
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await axios.get('https://7day35xrbospq4slpqt2abr7fq0mzeja.lambda-url.us-east-1.on.aws/');
+//         const responseData = response.data;
+
+//         // Store the received data in the state
+//         setData(responseData);
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     };
+
+//     fetchData();
+//   }, []);
+
+//   useEffect(() => {
+//     if (data.length === 0 || selectedY === '') {
+//       return; // Wait for data and selectedY to be set
+//     }
+  
+//     // Format the data for Plotly
+//     const plotlyData = {
+//       x: unpack(data, 'time'),
+//       y: unpack(data, selectedY),
+//       type: 'scatter',
+//       mode: 'lines'
+//     };
+
+//     setPlotData(plotlyData);
+//     console.log(plotData);
+  
+//   }, [data, selectedY]);
+  
+
+//   return (
+//     <div>
+//       <Autocomplete
+//         options={['temperature', 'humidity']} // Available options for y variable
+//         defaultValue='temperature'
+//         onChange={(event, value) => setSelectedY(value)}
+//         renderInput={(params) => <TextField {...params} label="Select Y Variable" />}
+//       />
+//       <Plot
+//         data={plotData}
+//         layout={{width: 320, height: 240}}
+//         onInitialized={(figure) => this.setState(figure)}
+//         onUpdate={(figure) => this.setState(figure)}
+//       />
+//     </div>
+//   );
+// };
+
+// export default Chart;
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Autocomplete, TextField } from "@mui/material";
-import styles from "./Chart.module.css";
+import Plot from 'react-plotly.js';
+
+function unpack(rows, key) {
+  return rows.map(function (row) {
+    return row[key];
+  });
+}
 
 const Chart = () => {
+  const [data, setData] = useState([]);
+  const [plotData, setPlotData] = useState([]);
+  const [selectedY, setSelectedY] = useState('temperature');
+
+  // Fetch data from the API
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://7day35xrbospq4slpqt2abr7fq0mzeja.lambda-url.us-east-1.on.aws/');
+        const responseData = response.data;
+
+        // Store the received data in the state
+        setData(responseData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (data.length === 0 || selectedY === '') {
+      return; // Wait for data and selectedY to be set
+    }
+
+    // Format the data for Plotly
+    const plotlyData = {
+      x: unpack(data, 'time'),
+      y: unpack(data, selectedY),
+      type: 'scatter',
+      mode: 'lines'
+    };
+
+    setPlotData([plotlyData]);
+  }, [data, selectedY]);
+
   return (
-    <div className={styles.lineCharttwovaluesParent}>
-      <div className={styles.lineCharttwovalues}>
-        <div className={styles.lineChart}>
-          <div className={styles.lineChartChild} />
-          <div className={styles.text}>Text</div>
-          <div className={styles.text1}>Text</div>
-          <div className={styles.text2}>Text</div>
-          <div className={styles.text3}>Text</div>
-          <div className={styles.text4}>Text</div>
-          <div className={styles.text5}>Text</div>
-          <div className={styles.text6}>Text</div>
-          <div className={styles.text7}>Text</div>
-          <div className={styles.text8}>Text</div>
-          <div className={styles.text9}>Text</div>
-          <div className={styles.text10}>Text</div>
-          <div className={styles.text11}>Text</div>
-          <div className={styles.div}>0</div>
-          <div className={styles.div1}>10</div>
-          <div className={styles.div2}>20</div>
-          <div className={styles.div3}>30</div>
-          <div className={styles.div4}>40</div>
-          <div className={styles.div5}>50</div>
-          <div className={styles.div6}>60</div>
-          <div className={styles.div7}>70</div>
-          <div className={styles.div8}>80</div>
-          <div className={styles.div9}>90</div>
-          <div className={styles.div10}>100</div>
-          <img className={styles.gridV1Icon} alt="" src="/grid-v-1.svg" />
-          <img className={styles.gridV2Icon} alt="" src="/grid-v-2.svg" />
-          <img className={styles.gridV3Icon} alt="" src="/grid-v-3.svg" />
-          <img className={styles.gridV4Icon} alt="" src="/grid-v-4.svg" />
-          <img className={styles.gridV5Icon} alt="" src="/grid-v-5.svg" />
-          <img className={styles.gridV6Icon} alt="" src="/grid-v-6.svg" />
-          <img className={styles.gridV7Icon} alt="" src="/grid-v-7.svg" />
-          <img className={styles.gridV8Icon} alt="" src="/grid-v-8.svg" />
-          <img className={styles.gridV9Icon} alt="" src="/grid-v-9.svg" />
-          <img className={styles.gridV10Icon} alt="" src="/grid-v-10.svg" />
-          <img className={styles.gridV11Icon} alt="" src="/grid-v-11.svg" />
-          <img className={styles.gridV12Icon} alt="" src="/grid-v-12.svg" />
-          <img className={styles.gridV1Icon1} alt="" src="/grid-v-13.svg" />
-          <img className={styles.gridV2Icon1} alt="" src="/grid-v-13.svg" />
-          <img className={styles.gridV3Icon1} alt="" src="/grid-v-13.svg" />
-          <img className={styles.gridV4Icon1} alt="" src="/grid-v-13.svg" />
-          <img className={styles.gridV5Icon1} alt="" src="/grid-v-13.svg" />
-          <img className={styles.gridV6Icon1} alt="" src="/grid-v-13.svg" />
-          <img className={styles.gridV7Icon1} alt="" src="/grid-v-13.svg" />
-          <img className={styles.gridV8Icon1} alt="" src="/grid-v-13.svg" />
-          <img className={styles.gridV9Icon1} alt="" src="/grid-v-13.svg" />
-          <img className={styles.gridV10Icon1} alt="" src="/grid-v-13.svg" />
-          <img className={styles.gridV11Icon1} alt="" src="/grid-v-13.svg" />
-          <img className={styles.line1Icon} alt="" src="/line-1.svg" />
-          <img className={styles.line2Icon} alt="" src="/line-2.svg" />
-        </div>
-      </div>
+    <div>
       <Autocomplete
-        className={styles.autocompletestandard}
-        disablePortal
-        options={[
-          "pH",
-          "Conductivity",
-          "Light Intensity",
-          "Temperature",
-          "Humidity",
-          "Oxygen",
-          "CO2",
-        ]}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            color="primary"
-            label="Label"
-            variant="standard"
-            placeholder=""
-            helperText=""
-          />
-        )}
-        defaultValue="pH"
-        size="medium"
+        options={['temperature', 'humidity', 'co2', 'oxygen', 'ph', 'conductivity']} // Available options for y variable
+        defaultValue='temperature'
+        onChange={(event, value) => setSelectedY(value)}
+        renderInput={(params) => <TextField {...params} label="Select Y Variable" />}
+      />
+      <Plot
+        data={plotData}
+        layout={{ width: 1000, height: 500 }}
+        onInitialized={(figure) => this.setState(figure)}
+        onUpdate={(figure) => this.setState(figure)}
       />
     </div>
   );
 };
 
 export default Chart;
+
---
+++ src/components/defaultData.js
@@ -0,0 +1,88 @@
+const defaultData = [
+{
+"Name": "capstone",
+"measure_name": "humidity",
+"time": new Date("2023-04-25T17:28:02.564Z"),
+"measure_value": 0
+},
+{
+"Name": "capstone",
+"measure_name": "co2",
+"time": new Date("2023-04-25T17:28:02.564Z"),
+"measure_value": 400
+},
+{
+"Name": "capstone",
+"measure_name": "lightIntensity",
+"time": new Date("2023-04-25T17:28:02.564Z"),
+"measure_value": 0
+},
+{
+"Name": "capstone",
+"measure_name": "conductivity",
+"time": new Date("2023-04-25T17:28:02.564Z"),
+"measure_value": 0
+},
+{
+"Name": "capstone",
+"measure_name": "temperature",
+"time": new Date("2023-04-25T17:28:02.564Z"),
+"measure_value": 0
+},
+{
+"Name": "capstone",
+"measure_name": "co2",
+"time": new Date("2023-04-25T16:28:01.161Z"),
+"measure_value": 2000
+},
+{
+"Name": "capstone",
+"measure_name": "temperature",
+"time": new Date("2023-04-25T16:28:01.161Z"),
+"measure_value": 50
+},
+{
+"Name": "capstone",
+"measure_name": "humidity",
+"time": new Date("2023-04-25T16:28:01.161Z"),
+"measure_value": 100
+},
+{
+    "Name": "capstone",
+    "measure_name": "lightIntensity",
+    "time": new Date("2023-04-25T16:28:01.161Z"),
+    "measure_value": 1000
+},
+{
+    "Name": "capstone",
+    "measure_name": "conductivity",
+    "time": new Date("2023-04-25T16:28:01.161Z"),
+    "measure_value": 2000
+},
+{
+    "Name": "capstone",
+    "measure_name": "pH",
+    "time": new Date("2023-04-25T17:28:02.564Z"),
+    "measure_value": 0
+},
+{
+    "Name": "capstone",
+    "measure_name": "pH",
+    "time": new Date("2023-04-25T17:28:02.564Z"),
+    "measure_value": 14
+},
+{
+    "Name": "capstone",
+    "measure_name": "oxygen",
+    "time": new Date("2023-04-25T17:28:02.564Z"),
+    "measure_value": 0
+},
+{
+    "Name": "capstone",
+    "measure_name": "oxygen",
+    "time": new Date("2023-04-25T17:28:02.564Z"),
+    "measure_value": 100
+}
+];
+
+export default defaultData;
